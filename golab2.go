2ef9f52c79eac566bb98e6e575969916a04766f6
JasonGodinho <jason_godinho@hotmail.com>
2015-10-21 22:38:25 -0700

    Go Lab2

---
+++ golab2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type MyJsonName struct {
+	Name string `json:"name"`
+}
+
+type MyJsonResponse struct {
+	Greeting string
+}
+
+func hello(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	fmt.Fprintf(rw, "Hello, %s!\n", p.ByName("name"))
+}
+
+func hellopost(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
+
+	var myjson3 MyJsonName
+	s3 := json.NewDecoder(req.Body)
+	err := s3.Decode(&myjson3)
+
+	var myJsonresp2 MyJsonResponse
+	myJsonresp2.Greeting = "Hello " + myjson3.Name + "!"
+	b2, err := json.Marshal(myJsonresp2)
+	if err != nil {
+	}
+	fmt.Fprintf(rw, string(b2))
+}
+
+func main() {
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+	mux.POST("/hello", hellopost)
+
+	server := http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
